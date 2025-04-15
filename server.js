3da0329c6a2b1931eb18ee1cb4efbdbd5aab8347
SweatSmartlyVOF <contact@sweatsmartly.com>
2025-04-15 12:43:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
-  res.send('SweatSmartly API draait! ✅');
+  res.send('SweatSmartly API draait!');
 });
 
 const PORT = process.env.PORT || 3000;
